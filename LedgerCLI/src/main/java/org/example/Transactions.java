1049dc5377608e280236d5df9eb33eadb82699ec
Freddie <freddie.j.canning@gmail.com>
2025-04-28 11:26:07 -0700

    Constructor created
    Constructor for ledgerCLI created

--- LedgerCLI/src/main/java/org/example/Transactions.java
+++ LedgerCLI/src/main/java/org/example/Transactions.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transactions {
+
+    private LocalTime time;
+    private LocalDate date;
+    String description;
+    String vendor;
+    double faceValue;
+
+    public Transactions(LocalTime time, LocalDate date, String description, String vendor, double faceValue) {
+        this.time = time;
+        this.date = date;
+        this.description = description;
+        this.vendor = vendor;
+        this.faceValue = faceValue;
+    }
 }
