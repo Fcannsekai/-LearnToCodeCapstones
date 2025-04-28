30d088c190d89846b91510fa3f6fd4f196ebb07a
Freddie <freddie.j.canning@gmail.com>
2025-04-28 12:52:48 -0700

    Home screen display
    added home screen, comments as well as a recursion method

--- LedgerCLI/src/main/java/org/example/Ledger.java
+++ LedgerCLI/src/main/java/org/example/Ledger.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Ledger {
+
+        private List<Transactions> transactionsList = new ArrayList<>();
+
+        public void homeScreen() { // homescreen display
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Welcome, what would you like to do today?\n" +
+                    "D) Add Deposit\n" +
+                    "P) Make Payment\n" +
+                    "L) Ledger\n" +
+                    "X) Exit\n" +
+                    ">");
+
+            String filePath = scanner.nextLine().trim().toUpperCase(); // for case sensitivity purposes
+
+            switch (filePath) {  // switch case instead of else if to improve readability and shorten code
+                case "D" -> System.out.println("test");
+                case "P" -> System.out.println("marq");
+                case "L" -> System.out.println("fred");
+                case "X" -> System.exit(0);
+                default -> System.out.println("Invalid input.");
+            }
+            homeScreen();
+        }
 }
