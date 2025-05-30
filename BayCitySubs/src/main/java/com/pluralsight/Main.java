a43392aabcfebfdf2bdc77f57b29a95eecfb61ac
Freddie <freddie.j.canning@gmail.com>
2025-05-29 19:37:40 -0700

    animation code
    animation code and plugged into the home screen as well as code fixes

--- BayCitySubs/src/main/java/com/pluralsight/Main.java
+++ BayCitySubs/src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,12 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.AnimationHomeStart;
 import com.pluralsight.ui.HomeScreen;
 
 public class Main {
     public static void main(String[] args) {
 
+        AnimationHomeStart.play();
         HomeScreen home = new HomeScreen();
         home.display();
 
