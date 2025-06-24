0fc2db670c4e546523f87d572c46fe596203977f
Freddie <freddie.j.canning@gmail.com>
2025-06-24 13:49:25 -0700

    updates
    update to categories controller with VAR

--- E-commerceAPI-Capstone3/src/main/java/org/yearup/controllers/CategoriesController.java
+++ E-commerceAPI-Capstone3/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -31,20 +33,30 @@ public class CategoriesController
         return categoryDao.getAll(); //temp errors going to be gone once i set up the DAO classes
     }
 
-    @GetMapping("{id}")  // GET http://localhost:8080/categories/{id}
-    public Category getById(@PathVariable int id)
-    {
-        return categoryDao.getById(id);
+    @GetMapping("{id}")  // GET {id}
+    public Category getById(@PathVariable int id) {
+        try {
+            var category = categoryDao.getById(id); //Method used to validate
+
+            if (category == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+            return category;
+        }
+        catch (ResponseStatusException ex) {   // changes response code -> 204 "Nothing here :(" "GET"
+            throw ex;
+        }
     }
 
-    @GetMapping("{categoryId}/products")  // GET http://localhost:8080/categories/{categoryId}/products
+    @GetMapping("{categoryId}/products")  // GET {categoryId}/products
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         return productDao.listByCategoryId(categoryId);
     }
 
-    @PostMapping  // POST http://localhost:8080/categories
-    @PreAuthorize("hasRole('ADMIN')")  // Only Admins can add categories
+    @PostMapping  // POST
+    @PreAuthorize("hasRole('ADMIN')")    // Only Admins can add categories
+    @ResponseStatus(HttpStatus.CREATED) // changes the response code -> 201 "POST"
     public Category addCategory(@RequestBody Category category)
     {
         return categoryDao.create(category);
@@ -58,8 +70,9 @@ public class CategoriesController
         categoryDao.update(category);
     }
 
-    @DeleteMapping("{id}")  // DELETE http://localhost:8080/categories/{id}
+    @DeleteMapping("{id}")  // DELETE {id}
     @PreAuthorize("hasRole('ADMIN')")  // Only Admins can delete categories
+    @ResponseStatus(HttpStatus.NO_CONTENT) //changes the response code -> 204 "DELETE"
     public void deleteCategory(@PathVariable int id)
     {
         categoryDao.delete(id);
