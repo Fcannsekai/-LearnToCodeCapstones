cb292de2b05afddb80ea1aa8832cf8bd67cf4eec
Freddie <freddie.j.canning@gmail.com>
2025-06-24 09:24:53 -0700

    implemented catagories controller
    this is not tested yet.

--- E-commerceAPI-Capstone3/src/main/java/org/yearup/controllers/CategoriesController.java
+++ E-commerceAPI-Capstone3/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,8 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,61 +10,58 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+@RestController                // Marks this class as a REST controller now can handle HTTP requests and return JSON
+@RequestMapping("/categories")  // Sets the base URL for this controller all methods will now respond to urls with /catagories
+@CrossOrigin  // Allows cross-origin requests to access my api and is necessary for front end testing
 public class CategoriesController
 {
-    private CategoryDao categoryDao;
-    private ProductDao productDao;
+    private final CategoryDao categoryDao;
+    private final ProductDao productDao;
 
+    @Autowired  // Constructor injection of the DAOs
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao)
+    {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
+    @GetMapping  // GET http://localhost:8080/categories
     public List<Category> getAll()
     {
-        // find and return all categories
-        return null;
+        return categoryDao.getAll(); //temp errors going to be gone once i set up the DAO classes
     }
 
-    // add the appropriate annotation for a get action
+    @GetMapping("{id}")  // GET http://localhost:8080/categories/{id}
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
+    @GetMapping("{categoryId}/products")  // GET http://localhost:8080/categories/{categoryId}/products
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
-        // get a list of product by categoryId
-        return null;
+        return productDao.getByCategoryId(categoryId);
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
+    @PostMapping  // POST http://localhost:8080/categories
+    @PreAuthorize("hasRole('ADMIN')")  // Only Admins can add categories
     public Category addCategory(@RequestBody Category category)
     {
-        // insert the category
-        return null;
+        return categoryDao.create(category);
     }
 
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("{id}")  
+    @PreAuthorize("hasRole('ADMIN')")  // Only Admins can update categories
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
-        // update the category by id
+        category.setCategoryId(id);
+        categoryDao.update(category);
     }
 
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("{id}")  // DELETE http://localhost:8080/categories/{id}
+    @PreAuthorize("hasRole('ADMIN')")  // Only Admins can delete categories
     public void deleteCategory(@PathVariable int id)
     {
-        // delete the category by id
+        categoryDao.delete(id);
     }
 }
